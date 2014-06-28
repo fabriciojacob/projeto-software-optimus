e4192c7ea3c12f99af76cf71dafc98aa6aaffa16
fabriciojacob124 <fabriciojacob124@gmail.com>
2014-06-28 17:17:01 +0000

    Share project "SoftwareOptimus" into "https://projeto-software-optimus.googlecode.com/svn/trunk/%20projeto-software-optimus"

---  projeto-software-optimus/SoftwareOptimus/src/br/com/softwareOptimus/util/DAOFactory.java
+++  projeto-software-optimus/SoftwareOptimus/src/br/com/softwareOptimus/util/DAOFactory.java
@@ -3,6 +3,7 @@ package br.com.softwareOptimus.util;
 import br.com.softwareOptimus.entidades.dao.usuario.UsuarioDAO;
 import br.com.softwareOptimus.entidades.dao.usuario.UsuarioDAOHibernate;
 
+
 public class DAOFactory {
 	
 	public static UsuarioDAO criaUsuarioDAO(){
